1df63872132ea46888663f62143337b3584cb0ff
Amirah <adamamirah09@gmail.com>
2022-06-18 04:30:52 +0800

    Fix UI for self scanning

--- frontenddermora/lib/screens/self_scanning/self_scanning_screen.dart
+++ frontenddermora/lib/screens/self_scanning/self_scanning_screen.dart
@@ -1,8 +1,12 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:tflite/tflite.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+import '../../util/styles.dart';
 
 class SelfScanningScreen extends StatefulWidget {
   const SelfScanningScreen({Key? key}) : super(key: key);
@@ -82,209 +86,230 @@ class _SelfScanningScreenState extends State<SelfScanningScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(30),
-                  bottomLeft: Radius.circular(30))),
-          backgroundColor: Colors.transparent,
-          centerTitle: true,
-          title: Text('Identify Your Skin Condition',
-              style: GoogleFonts.poppins(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.w400)),
-          flexibleSpace: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(30),
-                  bottomLeft: Radius.circular(30)),
-              gradient: const LinearGradient(
-                  colors: [
-                    Color(0xF04967FF),
-                    Color(0xFF8CC1F7),
-                  ],
-                  begin: const FractionalOffset(0.0, 0.4),
-                  end: Alignment.bottomLeft),
-            ),
-          )),
-      body: SingleChildScrollView(
-        //color: Colors.white,
-        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
-        child: Container(
-          alignment: Alignment.center,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                  child: Column(
-                children: [
-                  Text("DISCLAIMER:",
-                      style: GoogleFonts.poppins(
-                          color: Colors.redAccent,
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.w600)),
-                  SizedBox(
-                    height: 5,
-                  ),
-                  Text("The result may not be 100% accurate.",
-                      style: GoogleFonts.poppins(
-                        color: Colors.black,
-                        fontSize: 14.0,
-                      )),
-                  Text("Please consult with our Doctors",
-                      style: GoogleFonts.poppins(
-                        color: Colors.black,
-                        fontSize: 14.0,
-                      )),
-                  SizedBox(
-                    height: 30,
-                  ),
-                ],
-              )),
-              Container(
-                child: Center(
-                  child: _loading == true
-                      ? null //show nothing if no picture selected
-                      : Container(
-                          child: Column(
-                            children: [
-                              Container(
-                                height: MediaQuery.of(context).size.width * 0.6,
-                                width: MediaQuery.of(context).size.width * 0.6,
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(10),
-                                  child: Image.file(
-                                    _image,
-                                    fit: BoxFit.fill,
-                                  ),
+      body: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) => [
+                buildAppBar(),
+              ],
+          body: Scaffold(
+            body: SingleChildScrollView(
+              //color: Colors.white,
+              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 60),
+              child: Container(
+                alignment: Alignment.center,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                        child: Column(
+                      children: [
+                        Text("DISCLAIMER:",
+                            style: GoogleFonts.poppins(
+                                color: Colors.redAccent,
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w600)),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text("The result may not be 100% accurate.",
+                            style: GoogleFonts.poppins(
+                              color: Colors.black,
+                              fontSize: 14.0,
+                            )),
+                        Text("Please consult with our Doctors",
+                            style: GoogleFonts.poppins(
+                              color: Colors.black,
+                              fontSize: 14.0,
+                            )),
+                        SizedBox(
+                          height: 30,
+                        ),
+                      ],
+                    )),
+                    Container(
+                      child: Center(
+                        child: _loading == true
+                            ? Padding(
+                                padding: EdgeInsets.only(
+                                    top:
+                                        200)) //show nothing if no picture selected
+                            : Container(
+                                child: Column(
+                                  children: [
+                                    Container(
+                                      height:
+                                          MediaQuery.of(context).size.width *
+                                              0.6,
+                                      width: MediaQuery.of(context).size.width *
+                                          0.6,
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(10),
+                                        child: Image.file(
+                                          _image,
+                                          fit: BoxFit.fill,
+                                        ),
+                                      ),
+                                    ),
+                                    Divider(
+                                      height: 25,
+                                      thickness: 1,
+                                    ),
+                                    // ignore: unnecessary_null_comparison
+                                    _output != null
+                                        ? Text(
+                                            'Your skin condition might be: ${_output[0]['label']}',
+                                            style: GoogleFonts.poppins(
+                                              color: Colors.black,
+                                              fontSize: 14.0,
+                                            ))
+                                        : Container(),
+                                    Divider(
+                                      height: 25,
+                                      thickness: 1,
+                                    ),
+                                  ],
                                 ),
                               ),
-                              Divider(
-                                height: 25,
-                                thickness: 1,
-                              ),
-                              // ignore: unnecessary_null_comparison
-                              _output != null
-                                  ? Text(
-                                      'Your skin condition might be: ${_output[0]['label']}',
-                                      style: GoogleFonts.poppins(
-                                        color: Colors.black,
-                                        fontSize: 14.0,
-                                      ))
-                                  : Container(),
-                              Divider(
-                                height: 25,
-                                thickness: 1,
-                              ),
-                            ],
-                          ),
-                        ),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                child: Text("Select image from camera or gallery",
-                    style: GoogleFonts.poppins(
-                      color: Colors.black,
-                      fontSize: 14.0,
-                    )),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                child: Row(
-                  children: [
-                    GestureDetector(
-                      onTap: pickImage,
-                      child: Container(
-                          height: 50,
-                          width: 140,
-                          alignment: Alignment.center,
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 10),
-                          decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                              begin: Alignment.topLeft,
-                              end: Alignment.bottomRight,
-                              stops: [0.0, 1.0],
-                              colors: [
-                                Color(0xFF9DCEFF),
-                                Color(0XFF92A3FD),
-                              ],
-                            ),
-                            color: Colors.deepPurple.shade300,
-                            borderRadius: BorderRadius.circular(40),
-                          ),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Icon(
-                                Icons.camera_alt,
-                                color: Colors.white,
-                                size: 25,
-                              ),
-                              SizedBox(
-                                width: 5,
-                              ),
-                              Text("Camera",
-                                  style: GoogleFonts.poppins(
-                                    color: Colors.white,
-                                    fontSize: 16.0,
-                                  ))
-                            ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      child: Text("Select image from camera or gallery",
+                          style: GoogleFonts.poppins(
+                            color: Colors.black,
+                            fontSize: 14.0,
                           )),
                     ),
-                    SizedBox(width: 20),
-                    GestureDetector(
-                      onTap: pickGalleryImage,
-                      child: Container(
-                          height: 50,
-                          width: 140,
-                          alignment: Alignment.center,
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 10),
-                          decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                              begin: Alignment.topLeft,
-                              end: Alignment.bottomRight,
-                              stops: [0.0, 1.0],
-                              colors: [
-                                Color(0xFF9DCEFF),
-                                Color(0XFF92A3FD),
-                              ],
-                            ),
-                            color: Colors.deepPurple.shade300,
-                            borderRadius: BorderRadius.circular(40),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          GestureDetector(
+                            onTap: pickImage,
+                            child: Container(
+                                height: 50,
+                                width: 140,
+                                alignment: Alignment.center,
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 20, vertical: 10),
+                                decoration: BoxDecoration(
+                                  gradient: LinearGradient(
+                                    begin: Alignment.topLeft,
+                                    end: Alignment.bottomRight,
+                                    stops: [0.0, 1.0],
+                                    colors: [
+                                      Color(0xFF9DCEFF),
+                                      Color(0XFF92A3FD),
+                                    ],
+                                  ),
+                                  color: Colors.deepPurple.shade300,
+                                  borderRadius: BorderRadius.circular(40),
+                                ),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Expanded(
+                                      child: Icon(
+                                        Icons.camera_alt,
+                                        color: Colors.white,
+                                        size: 25,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 5,
+                                    ),
+                                    Text("Camera",
+                                        style: GoogleFonts.poppins(
+                                          color: Colors.white,
+                                          fontSize: 16.0,
+                                        ))
+                                  ],
+                                )),
                           ),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Icon(
-                                Icons.file_upload,
-                                color: Colors.white,
-                                size: 25,
-                              ),
-                              SizedBox(
-                                width: 5,
-                              ),
-                              Text("Gallery",
-                                  style: GoogleFonts.poppins(
-                                    color: Colors.white,
-                                    fontSize: 16.0,
-                                  ))
-                            ],
-                          )),
+                          SizedBox(width: 20),
+                          GestureDetector(
+                            onTap: pickGalleryImage,
+                            child: Container(
+                                height: 50,
+                                width: 140,
+                                alignment: Alignment.center,
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 20, vertical: 10),
+                                decoration: BoxDecoration(
+                                  gradient: LinearGradient(
+                                    begin: Alignment.topLeft,
+                                    end: Alignment.bottomRight,
+                                    stops: [0.0, 1.0],
+                                    colors: [
+                                      Color(0xFF9DCEFF),
+                                      Color(0XFF92A3FD),
+                                    ],
+                                  ),
+                                  color: Colors.deepPurple.shade300,
+                                  borderRadius: BorderRadius.circular(40),
+                                ),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Icon(
+                                      Icons.file_upload,
+                                      color: Colors.white,
+                                      size: 25,
+                                    ),
+                                    SizedBox(
+                                      width: 5,
+                                    ),
+                                    Text("Gallery",
+                                        style: GoogleFonts.poppins(
+                                          color: Colors.white,
+                                          fontSize: 16.0,
+                                        ))
+                                  ],
+                                )),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
               ),
-            ],
-          ),
-        ),
+            ),
+          )),
+    );
+  }
+
+  SliverAppBar buildAppBar() {
+    return SliverAppBar(
+      backgroundColor: Colors.white,
+      // Color(0xFFCCD9FD),
+      toolbarHeight: 90,
+      forceElevated: true,
+      elevation: 10,
+      shadowColor: kSecBlue,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+        bottomRight: Radius.circular(70),
+        bottomLeft: Radius.circular(70),
+      )),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Column(children: [
+            Text(
+              'Identify Your Skin Condition',
+              style: GoogleFonts.poppins(
+                color: Colors.black,
+                fontSize: 14.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ]),
+        ],
       ),
     );
   }
